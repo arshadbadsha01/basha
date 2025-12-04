a69610541f51c6a7b1ac02ffec61d06dd5adeffe
arshadbadsha01 <arshad.badsha01@gmail.com>
2025-12-04 23:19:11 +0530

    module 7 completed

--- Desktop/connect/backend/routes/posts.routes.js
+++ Desktop/connect/backend/routes/posts.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"; 
-import { activeCheck } from "../controller/posts.controller.js"; 
+import { activeCheck } from "../controllers/posts.controller.js"; 
 
 const router = Router(); 
 
--- Desktop/connect/backend/routes/user.routes.js
+++ Desktop/connect/backend/routes/user.routes.js
@@ -31,37 +31,41 @@
 // export default router; 
 
 
-import { Router } from "express";
-import { login, register, uploadProfilePicture, updateUserProfile, getUserProfile } from "../controller/user.controller.js";
-import multer from "multer";
+import { Router } from "express"; 
+import { login, register, uploadProfilePicture, updateUserProfile, getUserProfile, updateProfileData, getAllUserProfile, downloadProfile } from "../controllers/user.controller.js"; 
+import multer from "multer"; 
 
-const router = Router();
 
-const storage = multer.diskStorage({
+const router = Router(); 
 
-    destination: (req, file, cb) => {
-        cb(null, "uploads/");
+const storage = multer.diskStorage({ 
 
-    },
+  destination: (req, file, cb) => { 
+    cb(null, "uploads/"); 
 
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
+  }, 
 
-    },
+  filename: (req, file, cb) => { 
+    cb(null, file.originalname); 
 
-})
+  }, 
 
-const uploads = multer({ storage: storage });
+}) 
 
-router
 
-    .route("/update_profile_picture")
-    .post(uploads.single("profile_picture"), uploadProfilePicture);
+const uploads = multer({ storage: storage }); 
 
-router.route('/register').post(register);
-router.route("/login").post(login);
-router.route("/user_update").post(updateUserProfile);
-router.route("/get_user_and_profile").get(getUserProfile);
+router 
 
-export default router;
+  .route("/update_profile_picture") 
+  .post(uploads.single("profile_picture"), uploadProfilePicture, getAllUserProfile ); 
 
+router.route('/register').post(register); 
+router.route("/login").post(login); 
+router.route("/user_update").post(updateUserProfile); 
+router.route("/get_user_and_profile").get(getUserProfile); 
+router.route("/update_profile_data").post(updateProfileData); 
+router.route("/get_all_users").get(getAllUserProfile); 
+router.route("/download_resume").get(downloadProfile); 
+
+export default router;  
