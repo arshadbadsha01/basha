b77a47fbf7d47d3eed333e5152635fa648a5fe58
arshadbadsha01 <arshad.badsha01@gmail.com>
2025-12-05 22:40:49 +0530

    modules 9 and 10 completed

--- Desktop/connect/backend/controllers/posts.controller.js
+++ Desktop/connect/backend/controllers/posts.controller.js
@@ -1,5 +1,37 @@
-export const activeCheck = async (req, res) => { 
+import User from "../models/user.model.js";
+import Post from "../models/posts.model.js";
+import Profile from "../models/profile.model.js";
 
- return res.status(200).json({ message: "Running"}); 
+export const activeCheck = async (req, res) => {
+  return res.status(200).json({ message: "Running Perfectly" });
+};
 
+// To create a new Post
+
+export const createPost = async (req, res) => {
+  try {
+    const { token } = req.body;
+
+    const user = await User.findOne({ token: token });
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    const post = new Post({
+      userId: user._id,
+
+      body: req.body.body,
+
+      media: req.file != undefined ? req.file.filename : "",
+
+      fileType: req.file != undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+
+    await post.save();
+
+    return res.status(201).json({ message: "Post created Sucessfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
--- Desktop/connect/backend/routes/posts.routes.js
+++ Desktop/connect/backend/routes/posts.routes.js
@@ -1,8 +1,37 @@
-import { Router } from "express"; 
-import { activeCheck } from "../controllers/posts.controller.js"; 
+import { Router } from "express";
 
-const router = Router(); 
+import { activeCheck, createPost } from "../controllers/posts.controller.js";
 
-router.route("/").get(activeCheck); 
+import multer from "multer";
 
-export default router;
+const router = Router();
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, res, cb) => {
+    cb(null, res.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.route("/").get(activeCheck);
+
+router.route("/post").post(upload.single("media"), createPost);
+
+// router.route("/posts").get(getAllPosts);
+
+// router.route("/delete_post").post(deletePost);
+
+// router.route("/comment_post").post(commentPost);
+
+// router.route("/get_comment_by_post").get(getCommentsByPost);
+
+// router.route("/delete_comment_of_user").post(deleteCommentOfUser);
+
+// router.route("/update_like").post(updateLike);
+
+export default router;
