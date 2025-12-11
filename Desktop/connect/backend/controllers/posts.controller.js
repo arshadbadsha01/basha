d46087f5f89b2efe7fc9299ecd796dc199219bf3
arshadbadsha01 <arshad.badsha01@gmail.com>
2025-12-11 20:49:25 +0530

    module 11 completed

--- Desktop/connect/backend/controllers/posts.controller.js
+++ Desktop/connect/backend/controllers/posts.controller.js
@@ -87,3 +87,134 @@ export const deletePost = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+// To create a new comment on a Post
+
+export const commentPost = async (req, res) => {
+  try {
+    const { token, postId, commentBody } = req.body;
+
+    const user = await User.findOne({ token: token }).select("_id");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+
+    const post = await Post.findOne({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not Found" });
+    }
+
+    const comment = new Comment({
+      userId: user._id,
+
+      postId: postId,
+
+      body: commentBody,
+    });
+
+    await comment.save();
+
+    post.comments.push(comment._id);
+
+    await post.save();
+
+    return res.status(201).json({ message: "Comment created" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// To get all comment's of a specific post
+
+export const getCommentsByPost = async (req, res) => {
+  try {
+    const { postId } = req.body;
+
+    const post = await Post.findOne({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    return res.json({ comments: post.comments });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// To delete a specific comment of a user on a post
+
+export const deleteCommentOfUser = async (req, res) => {
+  try {
+    const { token, commentId, postId } = req.body;
+
+    const user = await User.findOne({ token: token });
+
+    if (!user) {
+      return res.status(404).json("User not Found");
+    }
+
+    const comment = await Comment.findOne({ _id: commentId });
+
+    if (!comment) {
+      return res.status(404).json({ message: "comment not Found" });
+    }
+
+    if (comment.userId.toString() != user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const post = await Post.findOne({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not Found" });
+    }
+
+    post.comments = post.comments.filter(
+      (id) => id.toString() !== commentId.toString()
+    );
+
+    await post.save();
+
+    await Comment.deleteOne({ _id: commentId });
+
+    return res.status(200).json({ message: "Comment Deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Update like of post (increase or decrease)
+
+export const updateLike = async (req, res) => {
+  try {
+    const { token, postId, type } = req.body;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await User.findOne({ token: token });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not Found" });
+    }
+
+    const post = await Post.findOne({ _id: postId });
+
+    if (!post) {
+      return res.status(404).json("Post not Found");
+    }
+
+    if (type === "increment") post.likes = post.likes + 1;
+    else post.likes = post.likes - 1;
+
+    await post.save();
+
+    return res.status(200).json({ message: "Like updated" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- Desktop/connect/backend/routes/posts.routes.js
+++ Desktop/connect/backend/routes/posts.routes.js
@@ -2,9 +2,13 @@ import { Router } from "express";
 
 import {
   activeCheck,
+  commentPost,
   createPost,
-  getAllPosts,
+  deleteCommentOfUser,
   deletePost,
+  getAllPosts,
+  getCommentsByPost,
+  updateLike,
 } from "../controllers/posts.controller.js";
 
 import multer from "multer";
@@ -31,12 +35,12 @@ router.route("/posts").get(getAllPosts);
 
 router.route("/delete_post").post(deletePost);
 
-// router.route("/comment_post").post(commentPost);
+router.route("/comment_post").post(commentPost);
 
-// router.route("/get_comment_by_post").get(getCommentsByPost);
+router.route("/get_comment_by_post").get(getCommentsByPost);
 
-// router.route("/delete_comment_of_user").post(deleteCommentOfUser);
+router.route("/delete_comment_of_user").post(deleteCommentOfUser);
 
-// router.route("/update_like").post(updateLike);
+router.route("/update_like").post(updateLike);
 
 export default router;
